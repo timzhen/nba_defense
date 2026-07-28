2f747bf97342b76fa52b251c4e45dc26db2c07c6
Tim Zhen <timzhen@ucsb.edu>
2026-07-28 13:47:39 -0700

    added dpoy_votes.csv

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def fetch_season_defense(season, max_retries=5, delay=15):
             stats = leaguedashplayerstats.LeagueDashPlayerStats(
                 season=season,
                 season_type_all_star='Regular Season',
-                measure_type_detailed_defense='Defense'
+                #measure_type_detailed_defense='Defense'
             )
             df = stats.get_data_frames()[0]
             if not df.empty:
